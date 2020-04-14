46886bf38e212f1d1dac1bb300ec6faa8d5c25e5
Cam Szarapka <cam.szarapka@eventtemple.com>
2020-04-14 01:54:59 -0700

    Handle parameter missing exception

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -4,6 +4,8 @@ module ExceptionHandler
   included do
     rescue_from ActiveRecord::RecordInvalid, with: :handle_unprocessable_entity
     rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found
+
+    rescue_from ActionController::ParameterMissing, with: :handle_bad_request
   end
 
   private
@@ -14,5 +16,9 @@ module ExceptionHandler
 
   def handle_not_found(e)
     render json: { errors: [{ status: 404, title: 'Not Found', detail: e.message }] }, status: 404
+  end
+
+  def handle_bad_request(e)
+    render json: { errors: [{ status: 400, title: 'Bad Request', detail: e.message }] }, status: 400
   end
 end
